e4076145778d6ebb3ee08b52df36fed47352e7b8
sonalitjadhav <sonalitjadhav@gmail.com>
2022-06-29 23:04:58 -0700

    Update JavaTest.java

--- java/JavaTestGit/src/com/test/JavaTest.java
+++ java/JavaTestGit/src/com/test/JavaTest.java
@@ -4,7 +4,7 @@ public class JavaTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Test Java");
+		System.out.println("Test Java Program upstream");
 	}
 
 }
